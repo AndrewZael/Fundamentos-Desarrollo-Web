6029e21a7d47580d93c3de136efb5982da09d2dc
AndrewZael <alfonsoguzman@live.cl>
2019-09-24 01:16:23 -0300

    Ajustes mobile y se creo menu slide

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
-// Tooltips
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+$(document).ready(function() {
+    // Tooltips
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+
+    // Smoth Scroll
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('#menu').removeClass('menu-show');
+            $('#btn-toogle-menu>i').removeClass('fa-times');
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+}); 
+
+// Menu
+function toogleMenu(){
+    $('#menu').toggleClass('menu-show');
+    $('#btn-toogle-menu>i').toggleClass('fa-times');
+}
